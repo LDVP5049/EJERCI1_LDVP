7db2a4beceeb5e2326f0eaaf68a7bba90e9c27d8
luism <luism@LUIS>
2023-10-03 20:30:06 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using EJERCI1_LDVP.metodos;
+using System.Xml;
+
+namespace EJERCI1_LDVP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicios eje1 = new Ejercicios();
+              eje1.CincoNum(); 
+
+          Ejercicios eje2 = new Ejercicios();
+              Console.WriteLine("Ingresa un numero");
+              int elem = Convert.ToInt32(Console.ReadLine());
+              eje2.CincoNum2(elem);
+
+              Ejercicios eje3 = new Ejercicios();
+              eje3.Media();
+
+            //AYUDA DE CHAT
+         
+            Console.WriteLine("Ingresa el tamaño del array");
+            int rango = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Numero para multiplos");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Ejercicios eje4 = new Ejercicios();
+            int[] arreglo = eje4.Arreglo(rango);
+
+            eje4.Multiplos(arreglo, numero);
+
+            eje4.MostrarArray(arreglo);*/
+
+            Console.Write("Ingrese el tamaño de los arrays: ");
+            int tamaño = Convert.ToInt32(Console.ReadLine());
+
+            Ejercicios eje5 = new Ejercicios();
+            string[] nombres = eje5.NombresArre(tamaño);
+            int[] longitudes = eje5.LongitudNombres(nombres);
+            for (int i = 0; i < tamaño; i++)
+            {
+                Console.Write($"Ingrese el nombre {i + 1}: ");
+                nombres[i] = Console.ReadLine();
+            }
+            longitudes = eje5.LongitudNombres(nombres);
+            eje5.NombresLong(nombres, longitudes);
+
+        }
+
+    }
+}
---
+++ metodos/Ejercicios.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCI1_LDVP.metodos
+{
+    internal class Ejercicios
+    {
+        public void CincoNum()
+        {
+            int[] n = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+
+        }
+
+        public void CincoNum2(int elem)
+        {
+            int cont = 0;
+            while (cont < elem)
+            {
+                cont++;
+                Console.WriteLine(cont);
+            }
+        }
+        public void Media()
+        {
+            int[] nuMedia = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int suma = 0;
+            for (int i = 0; i < nuMedia.Length; i++)
+            {
+                suma += nuMedia[i];
+            }
+            double media = suma / nuMedia.Length;
+            Console.WriteLine($"La media es {media}");
+        }
+
+
+        //AYUDA DE CHAT
+
+        public int[] Arreglo(int rango)
+        {
+            return new int[rango];
+        }
+
+        public void Multiplos(int[] arreglo, int numero)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = (i + 1) * numero;
+            }
+        }
+
+        public void MostrarArray(int[] arreglo)
+        {
+            Console.WriteLine("ARRAY:");
+            foreach (int elemento in arreglo)
+            {
+                Console.Write(elemento + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        //AYUDA DE CHAT/YOUTUBE -- MAL
+
+        public string[] NombresArre(int tamaño)
+        {
+            return new string[tamaño]; 
+        }
+
+        public int[] LongitudNombres(string[] nombres)
+        {
+            int[] longitudes = new int[nombres.Length];
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                longitudes[i] = nombres[i].Length;
+            }
+            return longitudes;
+        }
+
+        public void NombresLong(string[] nombres, int[] longitudes)
+        {
+            Console.WriteLine("Nombres y longitud");
+
+            for (int i = 0; i < nombres.Length ; i++)
+            {
+                Console.WriteLine($"nombre: {nombres}, longitudes {longitudes}");
+            }
+        }
+
+
+
+    }
+}
